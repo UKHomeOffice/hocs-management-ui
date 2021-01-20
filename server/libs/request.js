4e2fee1a79c31c9f4563e9e3e5d0a9dbf7021d35
Parth Vyas <parth.vyas@digital.homeoffice.gov.uk>
2021-01-20 11:51:34 +0000

    test

--- server/libs/request.js
+++ server/libs/request.js
@@ -5,22 +5,12 @@ const fs = require('fs');
 const isProduction = process.env.NODE_ENV === 'production';
 
 const getHttpsClient = () => {
-    try {
-        return new https.Agent({
-            cert: fs.readFileSync('/certs/tls.pem'),
-            key: fs.readFileSync('/certs/tls-key.pem'),
-            ca: fs.readFileSync('/etc/ssl/certs/ca-bundle.crt'),
-            rejectUnauthorized: false
-        });
-    } catch (e) {
-        process.on('uncaughtException', (err) => {
-            console.error('There was an uncaught error', err);
-            console.error('/certs:',fs.readdir('/certs'));
-            console.error('/etc/ssl/certs:',fs.readdir('/etc/ssl/certs'));
-            process.exit(1)
-        })
-    }
-    
+    return new https.Agent({
+        cert: fs.readFileSync('/certs/tls.pem'),
+        key: fs.readFileSync('/certs/tls-key.pem'),
+        ca: fs.readFileSync('/etc/ssl/certs/ca-certificates.crt'),
+        rejectUnauthorized: false
+    });
 };
 
 function createClient({ baseURL, auth }) {
