3fc83fcbd233b507570831c4936639ac98c3dc72
Gary Newbolt <gary.newbolt@digital.homeoffice.gov.uk>
2021-06-24 16:45:49 +0100

    HOCS-2995: remove <any> generic for new class

--- src/shared/common/components/successMessage.tsx
+++ src/shared/common/components/successMessage.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Location } from 'history';
 
+interface SuccessState {
+    successMessage: string;
+}
+
 interface SuccessMessageProps {
-    location: Location<any>;
+    location: Location<SuccessState>;
 }
 const successMessage: React.FC<SuccessMessageProps> = ({ location: { state: { successMessage } = {} } }) => successMessage ? (
     <div className="panel panel-border-wide alert-success">
