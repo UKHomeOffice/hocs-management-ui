0514f7fd28178405ec9bd9b9fdc211301c999ff5
Samuel Gardner <sam.gardner@digital.homeoffice.gov.uk>
2021-09-06 14:13:13 +0100

    HOCS-3239 Adding and testing management of MPAM business units

---
+++ src/shared/pages/list/mpamBusinessUnits/__tests__/businessAreaView.spec.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { match, MemoryRouter } from 'react-router-dom';
+import { createBrowserHistory, History, Location } from 'history';
+import { act, wait, render, RenderResult } from '@testing-library/react';
+import BusinessAreaView from '../businessAreaView';
+import * as ListService from '../../../../services/entityListService';
+import * as useError from '../../../../hooks/useError';
+
+let match: match<any>;
+let history: History<any>;
+let location: Location;
+
+jest.mock('../../../../services/entityListService', () => ({
+    __esModule: true,
+    getListItems: jest.fn().mockReturnValue(Promise.resolve({
+        data: [{
+            simpleName: 'testSimpleName1',
+            uuid: 'testId1',
+            title: 'testTitle1'
+        }, {
+            simpleName: 'testSimpleName2',
+            uuid: 'testId2',
+            title: 'testTitle2'
+        }]
+    }))
+}));
+
+const getListItemsSpy = jest.spyOn(ListService, 'getListItems');
+const useErrorSpy = jest.spyOn(useError, 'default');
+const setMessageSpy = jest.fn();
+const clearErrorsSpy = jest.fn();
+
+const renderComponent = () => render(
+    <MemoryRouter>
+        <BusinessAreaView history={history} location={location} match={match}></BusinessAreaView>
+    </MemoryRouter>
+);
+
+beforeEach(() => {
+    history = createBrowserHistory();
+    match = {
+        isExact: true,
+        params: {},
+        path: '',
+        url: ''
+    };
+
+    location = {
+        hash: '',
+        key: '',
+        pathname: '',
+        search: '',
+        state: {}
+    };
+    useErrorSpy.mockImplementation(() => [{}, jest.fn(), clearErrorsSpy, setMessageSpy]);
+    clearErrorsSpy.mockReset();
+    setMessageSpy.mockReset();
+});
+
+describe('when the businessAreaView component is mounted', () => {
+    it('should render with default props', async () => {
+        expect.assertions(2);
+        let wrapper: RenderResult;
+        act(() => {
+            wrapper = renderComponent();
+        });
+
+        await wait(() => {
+            expect(getListItemsSpy).toHaveBeenCalled();
+            expect(wrapper.container).toMatchSnapshot();
+        });
+    });
+});
---
+++ src/shared/pages/list/mpamBusinessUnits/selectBusinessArea.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { RouteComponentProps } from 'react-router';
+import { Link } from 'react-router-dom';
+import { string, object } from 'yup';
+import Submit from '../../../common/components/forms/submit';
+import { ApplicationConsumer } from '../../../contexts/application';
+import ErrorSummary from '../../../common/components/errorSummary';
+import useError from '../../../hooks/useError';
+import * as constants from '../../../models/constants';
+import { validate } from '../../../validation';
+import TypeAhead from '../../../common/components/typeAhead';
+import Item from '../../../models/item';
+
+interface ChooseBusinessAreaProps extends RouteComponentProps {
+    csrfToken?: string;
+}
+
+const validationSchema = object({
+    value: string()
+        .required()
+        .label('Business Area')
+});
+
+const ChooseBusinessArea: React.FC<ChooseBusinessAreaProps> = ({ csrfToken, history }) => {
+
+    const [pageError, addFormError, clearErrors] = useError('', constants.VALIDATION_ERROR_TITLE);
+
+    const [selectedCaseType, setSelectedCaseType] = React.useState<Item>();
+
+    const items: Item[] = [
+        { label: 'UKVI', value: 'MPAM_BUS_UNITS_1' },
+        { label: 'BF', value: 'MPAM_BUS_UNITS_2' },
+        { label: 'IE', value: 'MPAM_BUS_UNITS_3' },
+        { label: 'EUSS', value: 'MPAM_BUS_UNITS_4' },
+        { label: 'HMPO', value: 'MPAM_BUS_UNITS_5' },
+        { label: 'Windrush', value: 'MPAM_BUS_UNITS_6' },
+        { label: 'Corona Virus', value: 'MPAM_BUS_UNITS_7' },
+    ];
+
+    const getBusinessAreas = () => Promise.resolve(items);
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        clearErrors();
+        if (validate(validationSchema, selectedCaseType, addFormError)) {
+            history.push(`/business-area/${selectedCaseType!.value}`);
+        }
+    };
+
+    return (
+        <>
+            <div className="govuk-grid-row">
+                <div className="govuk-grid-column-two-thirds-from-desktop">
+                    <Link to="/" className="govuk-back-link">Back</Link>
+                    <ErrorSummary pageError={pageError} />
+                    <h1 className="govuk-heading-xl">
+                        Select a Business Area
+                    </h1>
+                </div>
+            </div>
+            <div className="govuk-grid-row">
+                <div className="govuk-grid-column-one-half-from-desktop">
+                    <form action="/api/template" method="POST" onSubmit={handleSubmit}>
+                        <input type="hidden" name="_csrf" value={csrfToken} />
+                        <TypeAhead
+                            clearable={true}
+                            disabled={false}
+                            getOptions={getBusinessAreas}
+                            label={'Business Areas'}
+                            name={'business-areas'}
+                            onSelectedItemChange={setSelectedCaseType}
+                            value={selectedCaseType}
+                        />
+                        <Submit />
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+};
+
+const WrappedChooseBusinessArea = ({ history, location, match }: RouteComponentProps) => (
+    <ApplicationConsumer>
+        {({ csrf }) => (
+            <ChooseBusinessArea csrfToken={csrf} history={history} location={location} match={match} />
+        )}
+    </ApplicationConsumer>
+);
+
+export default WrappedChooseBusinessArea;
