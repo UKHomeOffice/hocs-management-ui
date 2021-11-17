2ba81549031e36c830eb2be5b0feb3180c54a4c6
Joseph Rogerson <joseph.rogerson@digital.homeoffice.gov.uk>
2021-11-17 14:23:08 +0000

    HOCS-3639: Moved simpleName setter inline

--- src/shared/pages/list/mpamEnquiryReasons/addEnquiryReasonReducer.ts
+++ src/shared/pages/list/mpamEnquiryReasons/addEnquiryReasonReducer.ts
@@ -2,7 +2,6 @@ import EntityListItem from '../../../models/entityListItem';
 import InputEventData from '../../../models/inputEventData';
 
 export const reducer = (state: EntityListItem, inputEventData: InputEventData) => {
-    state.simpleName = inputEventData.value.toString();
-    const newState = { ...state, [inputEventData.name]: inputEventData.value };
+    const newState = { ...state, [inputEventData.name]: inputEventData.value, simpleName: inputEventData.value };
     return newState;
 };
